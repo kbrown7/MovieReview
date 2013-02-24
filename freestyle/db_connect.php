98a7cdd687634d45a46e1a42533dcf332418123c
Saad <sshami@mail.umw.edu>
2013-02-23 20:54:04 -0500

    Genre Page Changes
    - Added query to genre page to display movie along with genre in a table
    - Will add search bar to search by genre later
    - Added db_connect file
    - Changed all pages to link to genre.php instead of genre.html

---
+++ freestyle/db_connect.php
@@ -0,0 +1,5 @@
+<?php
+
+  	$db = mysqli_connect('localhost', 'root', NULL, 'movies')
+		or die (mysqli_error());
+?>
---
+++ freestyle/genre.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" xml:lang="en">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<meta name="description" content="Your description goes here" />
+	<meta name="keywords" content="your,keywords,goes,here" />
+	<meta name="author" content="Your Name" />
+	<link href='http://fonts.googleapis.com/css?family=Economica' rel='stylesheet' type='text/css' />
+	<link rel="stylesheet" type="text/css" href="freestyle.css" title="The Grid" media="all" />
+	<title>Freestyle v1.0</title>
+</head>
+
+<body class="wider orange">
+<div id="layout">
+
+	<div id="header" class="row">
+		<div class="col c12 aligncenter">
+			<h1><a class="red" href="index.html">Movie Ratings</a></h1>
+			<p class="slogan">Genre</p>
+		</div>
+	</div>
+		
+	<div class="row">
+	
+		<div class="col c2">
+			<ul class="menu">
+				<li><a href="index.html">Home</a></li>
+				<li><a href="topmovies.html">Top Movies</a></li>
+				<li><a href="actors.html">Actors</a></li>
+				<li><a href="genre.php">Genre</a></li>
+				<li><a href="ratings.html">New Ratings</a></li>
+				<li><a href="mpaa.html">MPAA Reviews</a></li>
+			</ul>
+		</div>
+		
+		<center><div>
+			<h2>Genres</h2>
+			<p>This page displays each movie according to genre</p>
+            <TABLE  BORDER="2" align="center" width="50%" cellpadding="4" cellspacing="3">
+   				<TR align="center">
+      				<TH COLSPAN="2">
+         				<H3><BR>Movie Genres</H3>
+      				</TH>
+   				</TR>
+      				<TH align="center">Movie</TH>
+      				<TH align="center">Genre</TH>
+                <?php
+                include "db_connect.php";
+                
+				$query = "SELECT movie_title,genre FROM movie GROUP BY genre";
+                
+				$result = mysqli_query($db, $query);
+				
+				while($row = mysqli_fetch_array($result)) {
+					$movie = $row['movie_title'];
+  					$genre = $row['genre'];	
+					echo "<TR align=\"center\">";
+					echo "<TD>".$movie."</TD>";
+					echo "<TD>".$genre."</TD>";
+					echo "</TR>";
+				}
+				
+				/*
+   				<TR align="center">
+      				<TD>Data 1</TD>
+      				<TD>Data 2</TD>
+   				</TR>
+   				<TR align="center">
+      				<TD>Data 3</TD>
+      				<TD>Data 4</TD>
+   				</TR>
+   				<TR align="center">
+      				<TD>Data 5</TD>
+      				<TD>Data 6</TD>
+   				</TR>
+                */
+				
+                ?>
+			</TABLE>
+            
+            
+		</div></center>
+        
+        
+
+		<div class="col c5 alignjustify">
+			
+            
+            
+		</div>
+	</div>
+
+	<div class="row separator">
+	</div>
+	<div id="footer" class="row">
+	  <div class="col c12">
+			<p>&copy; 2012 Your Name<br />
+			<a href="http://andreasviklund.com/templates/freestyle/">Template design</a> by <a href="http://andreasviklund.com/">Andreas Viklund</a><br /><span class="smaller">Best hosted at <a href="https://www.svenskadomaner.se/?ref=mall&amp;ling=en" title="Svenska Domäner AB">www.svenskadomaner.se</a></span></p>
+		</div>
+	</div>
+</div>
+</body>
+</html>
